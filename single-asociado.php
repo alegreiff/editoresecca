6f616606009b649bae29d86520000052ac6cbb50
Jaime de Greiff <alegreiff@gmail.com>
2019-04-11 10:38:03 -0500

    entrecortes enn single editor

--- single-asociado.php
+++ single-asociado.php
@@ -2,6 +2,31 @@
 
 $campos = get_fields();
 
+function tiene_entrecortes($id)
+{
+    $entrecortes = get_posts(array(
+        'post_type' => 'post',
+        'meta_query' => array(
+            array(
+                'key' => 'cortes_asociados', // name of custom field
+                'value' => '"' . $id . '"', // matches exactly "123", not just 123. This prevents a match for "1234"
+                'compare' => 'LIKE'
+            )
+        )
+    ));
+    if ($entrecortes) : ?>
+<ul>
+    <?php foreach ($entrecortes as $doctor) : ?>
+    <li>
+        <a href="<?php echo get_permalink($doctor->ID); ?>">
+
+            <?php echo get_the_title($doctor->ID); ?>
+        </a>
+    </li>
+    <?php endforeach; ?>
+</ul>
+<?php endif;
+}
 
 function imagensocio()
 {
@@ -41,13 +66,13 @@ function asociado_single_view()
         echo genesis_get_custom_field('asociado_cargoecca') .' ECCA';
     }
     echo '</h5>';
-    echo '<h5 class="tipo">' . genesis_get_custom_field('asociado_tipo') . '</h5>';
+    echo '<h5 class="tipo">Socio ' . genesis_get_custom_field('asociado_tipo') . ' ECCA</h5>';
 
     imagensocio();
     echo '<div class="asociado_bio">'.$post->post_content.'</div>';
     do_action('genesis_entry_footer');
 
-    
+    tiene_entrecortes($post->ID);
     echo '</div>';
 
     do_action('genesis_after_entry');
